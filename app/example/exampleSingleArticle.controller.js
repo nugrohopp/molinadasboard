9c5df6b30a51d86623ddab520d12e45913ff8ad0
Vitradisa Pratama <Vitradisa Pratama>
2016-04-25 23:52:06 +0700

    add example services

--- app/example/exampleSingleArticle.controller.js
+++ app/example/exampleSingleArticle.controller.js
@@ -3,45 +3,15 @@
     .module('app')
     .controller('exampleSingleArticle', exampleSingleArticle);
 
-  function exampleSingleArticle($stateParams) {
+  function exampleSingleArticle($stateParams,exampleService) {
     var expSArticle = this;
     expSArticle.articleID = $stateParams.article_id;
-    expSArticle.datas = [
-      {
-        "id":"1",
-        "title":"Article 1",
-        "content":"<p align='justify'>Appropriately develop multidisciplinary bandwidth vis-a-vis strategic convergence. Intrinsicly harness accurate potentialities rather than economically sound niche markets. Authoritatively facilitate technically sound deliverables via resource sucking paradigms. Credibly envisioneer.<br />Appropriately develop multidisciplinary bandwidth vis-a-vis strategic convergence. Intrinsicly harness accurate potentialities rather than economically sound niche markets. Authoritatively facilitate technically sound deliverables via resource sucking paradigms. Credibly envisioneer.</p>"
-      },
-      {
-        "id":"2",
-        "title":"Article 2",
-        "content":"<p align='justify'>Appropriately develop multidisciplinary bandwidth vis-a-vis strategic convergence. Intrinsicly harness accurate potentialities rather than economically sound niche markets. Authoritatively facilitate technically sound deliverables via resource sucking paradigms. Credibly envisioneer.<br />Appropriately develop multidisciplinary bandwidth vis-a-vis strategic convergence. Intrinsicly harness accurate potentialities rather than economically sound niche markets. Authoritatively facilitate technically sound deliverables via resource sucking paradigms. Credibly envisioneer.</p>"
-      },
-      {
-        "id":"3",
-        "title":"Article 3",
-        "content":"<p align='justify'>Appropriately develop multidisciplinary bandwidth vis-a-vis strategic convergence. Intrinsicly harness accurate potentialities rather than economically sound niche markets. Authoritatively facilitate technically sound deliverables via resource sucking paradigms. Credibly envisioneer.<br />Appropriately develop multidisciplinary bandwidth vis-a-vis strategic convergence. Intrinsicly harness accurate potentialities rather than economically sound niche markets. Authoritatively facilitate technically sound deliverables via resource sucking paradigms. Credibly envisioneer.</p>"
-      },
-      {
-        "id":"4",
-        "title":"Article 4",
-        "content":"<p align='justify'>Appropriately develop multidisciplinary bandwidth vis-a-vis strategic convergence. Intrinsicly harness accurate potentialities rather than economically sound niche markets. Authoritatively facilitate technically sound deliverables via resource sucking paradigms. Credibly envisioneer.<br />Appropriately develop multidisciplinary bandwidth vis-a-vis strategic convergence. Intrinsicly harness accurate potentialities rather than economically sound niche markets. Authoritatively facilitate technically sound deliverables via resource sucking paradigms. Credibly envisioneer.</p>"
-      },
-      {
-        "id":"5",
-        "title":"Article 5",
-        "content":"<p align='justify'>Appropriately develop multidisciplinary bandwidth vis-a-vis strategic convergence. Intrinsicly harness accurate potentialities rather than economically sound niche markets. Authoritatively facilitate technically sound deliverables via resource sucking paradigms. Credibly envisioneer.<br />Appropriately develop multidisciplinary bandwidth vis-a-vis strategic convergence. Intrinsicly harness accurate potentialities rather than economically sound niche markets. Authoritatively facilitate technically sound deliverables via resource sucking paradigms. Credibly envisioneer.</p>"
-      },
-      {
-        "id":"6",
-        "title":"Article 6",
-        "content":"<p align='justify'>Appropriately develop multidisciplinary bandwidth vis-a-vis strategic convergence. Intrinsicly harness accurate potentialities rather than economically sound niche markets. Authoritatively facilitate technically sound deliverables via resource sucking paradigms. Credibly envisioneer.<br />Appropriately develop multidisciplinary bandwidth vis-a-vis strategic convergence. Intrinsicly harness accurate potentialities rather than economically sound niche markets. Authoritatively facilitate technically sound deliverables via resource sucking paradigms. Credibly envisioneer.</p>"
-      },
-      {
-        "id":"7",
-        "title":"Article 7",
-        "content":"<p align='justify'>Appropriately develop multidisciplinary bandwidth vis-a-vis strategic convergence. Intrinsicly harness accurate potentialities rather than economically sound niche markets. Authoritatively facilitate technically sound deliverables via resource sucking paradigms. Credibly envisioneer.<br />Appropriately develop multidisciplinary bandwidth vis-a-vis strategic convergence. Intrinsicly harness accurate potentialities rather than economically sound niche markets. Authoritatively facilitate technically sound deliverables via resource sucking paradigms. Credibly envisioneer.</p>"
-      }
-    ];
+
+    exampleService.getAllData().then(
+			function(allData) {
+				expSArticle.datas = allData;
+			}
+		);
+    
   }
 })();
