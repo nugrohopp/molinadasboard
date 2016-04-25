e1d77613f6c6eba9b6128c0cf889ac2702bed274
Vitradisa Pratama <Vitradisa Pratama>
2016-04-25 23:26:26 +0700

    add 360 slider

--- app/spareparts/spareparts.controller.js
+++ app/spareparts/spareparts.controller.js
@@ -5,6 +5,21 @@
 
   function spareparts() {
     var s = this;
-
+    $(document).ready(function(){
+      var mobilmuter = $('.mobilmuter').ThreeSixty({
+            totalFrames: 52, // Total no. of image you have for 360 slider
+            endFrame: 30, // end frame for the auto spin animation
+            currentFrame: 1, // This the start frame for auto spin
+            imgList: '.threesixty_images', // selector for image list
+            progress: '.spinner', // selector to show the loading progress
+            imagePath:'assets/car/', // path of the image assets
+            filePrefix: '', // file prefix if any
+            ext: '.png', // extention for the assets
+            height: 420,
+            width: 977,
+            navigation: true,
+            disableSpin: false // Default false
+        });
+      });
   }
 })();
