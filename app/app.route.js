5304c5777a0163fc9d5959f40b88c35dc2209738
Vitradisa Pratama <vita.reinforce@gmail.com>
2016-05-17 12:45:17 +0700

    add default state params

--- app/app.route.js
+++ app/app.route.js
@@ -5,12 +5,18 @@
     .config(router);
 
   function router($urlRouterProvider, $stateProvider) {
-    $urlRouterProvider.otherwise("/mobil/1");
+    $urlRouterProvider.otherwise("/");
     $stateProvider.
         state('root', {
           url:'/',
           templateUrl: 'app/mobil/mobil.html',
-          controller: 'mobil'
+          controller: 'mobil',
+          onEnter: function ($state, $stateParams) {
+            console.log($stateParams.mobil_id);
+            if (!$stateParams.mobil_id) {
+              $stateParams.mobil_id = 1;
+            }
+          },
     });
   }
 })();
